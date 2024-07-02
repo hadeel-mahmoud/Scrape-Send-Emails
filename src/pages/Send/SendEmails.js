22505f57467b9d20090b14730d16d32c46ba1874
HadeelNez <nez.hadeel@gmail.com>
2024-07-02 23:54:18 +0400

    Send Emails UI

--- src/pages/Send/SendEmails.js
+++ src/pages/Send/SendEmails.js
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import { Button } from "../../components/Button/Button";
+import { Dropdown } from "../../components/Dropdown/Dropdown";
 import Wrapper from "../../components/Wrapper/Wrapper";
 import styles from "./SendEmails.module.css";
 
 export const SendEmails = () => {
+  const categories = [
+    { category: "All Communities", URLs: [] },
+    { category: "Podcasts", URLs: ["www.google.com"] },
+    { category: "Skool", URLs: [] },
+    { category: "Podcasts", URLs: [] },
+  ];
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
   function handleSendClick() {
     //connect to API
   }
   return (
     <Wrapper>
+      <label className={styles.labels}>Send Emails To</label>
+      <Dropdown
+        onDropdownChange={(value) => setSelectedCategory(value)}
+        data={categories}
+        placeholder={"Choose Community"}
+        attribute="category"
+      />
+      <label className={styles.labels}>Email Body</label>
+
+      <textarea
+        rows={18}
+        placeholder="Enter the email content..."
+        className={styles.emailBody}
+      />
+
       <Button onClick={handleSendClick} buttonLabel="Send Emails" />
     </Wrapper>
   );
