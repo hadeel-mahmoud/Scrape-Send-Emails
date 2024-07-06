2a64e53ba3d3bef9601526af5913a304e31f2451
HadeelNez <nez.hadeel@gmail.com>
2024-07-06 20:37:22 +0400

    some refactoring

--- backend/src/routes/scrapeEmails.js
+++ backend/src/routes/scrapeEmails.js
@@ -2,37 +2,34 @@ var express = require("express");
 var axios = require("axios");
 const Email = require("../models/Email");
 var router = express.Router();
-router.post("/scrape-url", async (req, res) => {
+router.post("/insert-url-scraped-emails", async (req, res) => {
   try {
-    await axios(
+    const response = await axios(
       "https://raw.githubusercontent.com/ageitgey/all-podcasts-dataset/master/a.tsv"
-    ).then(async (response) => {
-      const html_data = response.data;
+    );
+    const { communityTypeID } = req.body;
+    const html_data = response.data;
+    // ^ => start, $ => end , \w => words , + => one or more, [\w-]{2,4} => match sequence of 2 to 4 consecutive characters or hypens e g=> .com/.net
+    const emailPattern = /[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/g;
+    // match into set so that we make sure there are no email duplicates =>
+    const emailAddresses = new Set(html_data.match(emailPattern) || []);
+    // add attributes to email object before creating
+    const emailObjects = Array.from(emailAddresses).map((email) => ({
+      email,
+      communityTypeID,
+    }));
 
-      // ^ => start, $ => end , \w => words , + => one or more, [\w-]{2,4} => match sequence of 2 to 4 consecutive characters or hypens e g=> .com/.net
-      const emailPattern = /[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/g;
-      // match into set so that we make sure there are no email duplicates =>
-      const emailAddresses = new Set(html_data.match(emailPattern) || []);
-      // add attributes to email object before creating
-      const emailObjects = Array.from(emailAddresses).map((email) => ({
-        email: email,
-        communityTypeID: req.body.communityTypeID,
-      }));
-
-      const bulkOperations = emailObjects.map((emailObject) => ({
-        updateOne: {
-          filter: { email: emailObject.email },
-          update: { $setOnInsert: emailObject },
-          upsert: true,
-        },
-      }));
-      await Email.bulkWrite(bulkOperations);
-      res.status(201).send({ message: "Records inserted successfully" });
-    });
+    const bulkOperations = emailObjects.map((emailObject) => ({
+      updateOne: {
+        filter: { email: emailObject.email },
+        update: { $setOnInsert: emailObject },
+        upsert: true,
+      },
+    }));
+    await Email.bulkWrite(bulkOperations);
+    res.status(201).send({ message: "Records inserted successfully" });
   } catch (e) {
-    console.log("fail");
-    console.log(e);
-    res.sendStatus(500);
+    res.status(500).send({ error: `${e} : "Failed to insert records` });
   }
 });
 
